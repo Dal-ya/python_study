c4834f0e24e1af99ff6ccd079bbed4dac10bb6bb
mac <>
2018-10-07 10:47:21 +0900

    css studied

--- feed/models.py
+++ feed/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    view_count = models.IntegerField(default=0)
+    pub_date = models.DateTimeField('data published')
+
+    def __str__(self):
+        return self.title
+    
+
+class Comment(models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=150)
+    cmt_date = models.DateTimeField('data published')
+
+    def __str__(self):
+        return self.comment
+    
--- feed/views.py
+++ feed/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+#from django.http import HttpResponse
+from feed.models import Article, Comment
 
-# Create your views here.
+
+
+# def index(request):
+#     return HttpResponse("Hello, world. You're at the feed index.")
+
+
+# def index(request):
+#     name = "Sherlock"
+#     num = [1,2,3,4,5]
+
+#     return render(request, "index.html", {"name":name, "num":num})
+
+
+def index(request):
+    article_list = Article.objects.all()
+    ctx = {
+        "article_list" : article_list
+    }
+    return render(request, "index.html", ctx)
